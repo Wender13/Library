da7724fa6bfc370a98a387ca8948749b91664027
Wender <w3nd3r.jr62@gmail.com>
2022-03-23 07:51:21 -0300

    Design almost done

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const addBook = document.getElementById('addBook')
+const goToTop = document.getElementById('goToTop')
+
 let myLibrary = [];
 
 function Book() {
@@ -7,3 +10,20 @@ function Book() {
 function addBookToLibrary() {
     
 }
+
+function bookInput() {
+    const bookInputContainer = document.createElement('div')
+    const bookInput = document.getElementById('bookInput')
+    bookInputContainer.classList.add('addBookContainer')
+    bookInput.classList.add('bookInput')
+    bookInput.appendChild(bookInputContainer)
+}
+
+addBook.addEventListener('click', bookInput)
+
+goToTop.addEventListener('click', function () {
+    window.scrollTo({
+        top:0,
+        behavior: 'smooth'
+    })
+})
